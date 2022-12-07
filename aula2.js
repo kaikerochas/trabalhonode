bda12712960671f9f1ab433c50205ebd418052eb
Kaike Rocha <114085750+kaikerochas@users.noreply.github.com>
2022-12-07 09:27:22 -0300

    adição da variável dia

--- aula2.js
+++ aula2.js
@@ -3,7 +3,8 @@ var url = require('url');
 
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(req, url);
     var q = url.parse(req.url, true).query;
-    var txt = "ano: " +q.ano+"mes: "+q.mes;
+    var txt = "ano: " +q.ano+"mes: "+q.mes+"dia: "+q.dia;
     res.end(txt);
 }).listen(80);
